4ccdbfa16add4db2ab265e6e40956331ce2ec243
DESKTOP-KU34JC9 <thongminhtranle123@gmail.com>
2026-04-05 14:26:26 +0700

    fix deploy

--- backend/Models/WebBanDoChoiContext.cs
+++ backend/Models/WebBanDoChoiContext.cs
@@ -28,11 +28,11 @@ namespace DoAn_WebBanDoChoi.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=WebBanDoChoi;Username=postgres;Password=123456");
-            }
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=WebBanDoChoi;Username=postgres;Password=123456");
+//            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- backend/Program.cs
+++ backend/Program.cs
@@ -21,7 +21,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<WebBanDoChoiContext>();
+    db.Database.Migrate();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
