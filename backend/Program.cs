bde4271da1960038d858620cafad72d2deef8dad
DESKTOP-KU34JC9 <thongminhtranle123@gmail.com>
2026-04-08 00:56:14 +0700

    fix deploy project

--- backend/Program.cs
+++ backend/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
             "http://localhost:5173", 
             "http://localhost:3000",
             "https://webbandochoi.onrender.com",
-            "https://*.onrender.com"
+            "https://webbandochoi.netlify.app"
         )
               .AllowAnyMethod()
               .AllowAnyHeader()
